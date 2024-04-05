3c65541de99c7c410602e03d2f9db66d4cb432d2
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-04-05 19:31:05 +0300

    Project: Update main to keras

--- project/src/main.py
+++ project/src/main.py
@@ -1,24 +1,20 @@
+#!/usr/bin/env bash
+
+import os
+
+os.environ["KERAS_BACKEND"] = "torch"
+
 import typing as t
-from torch import Tensor
-from torch.utils.data.dataloader import DataLoader
+import keras
 from data.dataset import ImageDataset
-from data.cacheable_tensor_dataset import CacheableTensorDataset
 from utils.args import parse_arguments
-from utils.device import get_default_device
 
 
 def main():
     args = parse_arguments()
-    device = get_default_device()
-    transforms: t.Callable[[Tensor], Tensor] = []
+    transforms: t.Callable[[keras.KerasTensor], keras.KerasTensor] = []
 
     dataset = ImageDataset(data_path="./dataset", transforms=transforms)
-    cached_dataset = CacheableTensorDataset(dataset=dataset, cache=True)
-    batched_image_dataloader = DataLoader(
-        dataset=cached_dataset,
-        batch_size=len(cached_dataset) if args.batch_size == 0 else args.batch_size,
-        shuffle=True,
-    )
 
     # TODO: implement the rest:
     #    1. add image transformations to enhance accuracy (ideas: remove background, extract eyes only)
