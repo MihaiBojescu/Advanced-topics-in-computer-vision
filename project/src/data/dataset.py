8383d0ab9f6c70e04a5776d0d790679632d23564
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-05-17 19:10:37 +0300

    Make dataset more generic

--- project/src/data/dataset.py
+++ project/src/data/dataset.py
@@ -1,8 +1,6 @@
 import csv
 import typing as t
 from dataclasses import dataclass
-
-import keras
 from keras.preprocessing.image import load_img
 
 
@@ -22,41 +20,50 @@ class Label:
     y: int
 
 
-class ImageDataset:
-    __transforms: t.Optional[t.Callable[[keras.KerasTensor], keras.KerasTensor]]
-    __data: t.List[keras.KerasTensor]
-    __labels: t.List[Label]
+T = t.TypeVar("T")
+U = t.TypeVar("U")
+L = t.TypeVar("L")
+
+
+class ImageDataset(t.Generic[T, U]):
+    __transforms: t.Optional[t.Callable[[T], U]]
+    __label_transforms: t.Optional[t.Callable[[T], U]]
+    __data: t.List[T]
+    __labels: t.List[L]
     __metas: t.List[Metadata]
 
     def __init__(
         self,
         *args,
-        data_file_path: str,
-        transforms: t.Optional[
-            t.Callable[[keras.KerasTensor], keras.KerasTensor]
-        ] = None,
+        data_path: str,
+        data_file: t.Optional[str],
+        transforms: t.Optional[t.Callable[[T], U]] = None,
+        label_transforms: t.Optional[t.Callable[[L], L]] = None,
     ):
         super().__init__(*args)
 
         self.__transforms = transforms
-        self.__data, self.__labels, self.__metas = self.__load_data(data_file_path=data_file_path)
+        self.__label_transforms = label_transforms
+        self.__data, self.__labels, self.__metas = self.__load_data(
+            data_path=data_path, data_file=data_file
+        )
 
     def __load_data(
-        self, data_file_path: str
-    ) -> t.Tuple[t.List[keras.KerasTensor], t.List[Label], t.List[Metadata]]:
-        data: t.List[t.Tuple[str, keras.KerasTensor]] = []
-        labels: t.List[Label] = []
+        self, data_path: str, data_file: t.Optional[str]
+    ) -> t.Tuple[t.List[T], t.List[L], t.List[Metadata]]:
+        data: t.List[T] = []
+        labels: t.List[L] = []
         metas: t.List[Metadata] = []
-        data_file = data_file_path or f'{data_file_path}/train.csv'
+        data_file = data_file if data_file is not None else "train.csv"
 
-        with open(data_file) as file:
+        with open(f"{data_path}/{data_file}") as file:
             reader = csv.reader(file)
             next(reader)
 
             for row in reader:
                 filename = row[0]
-                x = int(row[1])
-                y = int(row[2])
+                x = int(float(row[1]))
+                y = int(float(row[2]))
                 width = int(row[3])
                 height = int(row[4])
                 distance = float(row[5])
@@ -65,19 +72,24 @@ class ImageDataset:
                 metas.append(Metadata(filename, x, y, width, height, distance))
 
         for entry in metas:
-            tensor = load_img(f"{data_file_path}/images/{entry.filename}")
-            data.append(tensor)
+            image = load_img(f"{data_path}/images/{entry.filename}")
+            data.append(image)
 
         return data, labels, metas
 
     def __len__(self) -> int:
         return len(self.__data)
 
-    def __getitem__(self, index) -> t.Tuple[keras.KerasTensor, Label]:
-        tensor = self.__data[index]
+    def __getitem__(self, index) -> t.Tuple[U, L]:
+        data = self.__data[index]
         label = self.__labels[index]
 
         if self.__transforms:
-            tensor = self.__transforms(tensor)
+            for transform in self.__transforms:
+                data = transform(data)
 
-        return tensor, label
+        if self.__label_transforms:
+            for transform in self.__transforms:
+                label = transform(label)
+
+        return data, label
--- project/src/main.py
+++ project/src/main.py
@@ -6,21 +6,27 @@ os.environ["KERAS_BACKEND"] = "torch"
 
 import typing as t
 import keras
+from PIL.Image import Image
 
 from data.dataset import ImageDataset
-from data.transforms import grayscale_transform, normalize_coordinates
+from data.transforms import Padding, grayscale_transform, normalize_coordinates
 
 from utils.args import parse_arguments
 
 
-
 def main():
     args = parse_arguments()
-    transforms: t.Callable[[keras.KerasTensor], keras.KerasTensor] = [grayscale_transform]
+    transforms = [grayscale_transform]
 
-    train_dataset = ImageDataset(data_file_path="./dataset/train.csv", transforms=transforms)
-    validation_dataset = ImageDataset(data_file_path="./dataset/validation.csv", transforms=transforms)
-    test_dataset = ImageDataset(data_file_path="./dataset/test.csv", transforms=transforms)
+    train_dataset = ImageDataset(
+        data_path="./dataset", data_file="train.csv", transforms=transforms
+    )
+    validation_dataset = ImageDataset(
+        data_path="./dataset", data_file="validation.csv", transforms=transforms
+    )
+    test_dataset = ImageDataset(
+        data_path="./dataset", data_file="test.csv", transforms=transforms
+    )
 
     normalized_labels = [normalize_coordinates(label, (img.width, img.height)) for img, label in zip(dataset.__data, dataset.__labels)]
     dataset.__labels = normalized_labels
@@ -32,6 +38,7 @@ def main():
     #    3. add neural network
     #    4. add client-side code for Windows/Linux/MacOS
     #    5. ...
+
 
 if __name__ == "__main__":
     main()
